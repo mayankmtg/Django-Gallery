72bb5afe33dabba6d89fe539f30fa9ecefcee38b
mayankmtg <mayank15056@iiitd.ac.in>
2016-12-22 15:35:13 +0530

    separating html code from python

--- paintings/views.py
+++ paintings/views.py
@@ -1,8 +1,21 @@
 from django.http import HttpResponse
+from django.template import loader
+from .models import Segment
 
 def index(request):
-	return HttpResponse("<h1>This is the Paintings Section</h1>")
+
+	all_segments = Segment.objects.all()
+	template = loader.get_template('paintings/index.html')
+	
+	#now we need a dictionary to pass tha data since 
+	#the django interface of teh loader works only with the dictionary
+
+	#generally the dictionary is called the context
+	context = {
+		'all_segments' : all_segments,
+	}
+
+	return HttpResponse(template.render(context, request))
 
 def nextseg(request, segment_id):
 	return HttpResponse("<h2>This is Segment Number: " + str(segment_id) + "</h2>")
-	
